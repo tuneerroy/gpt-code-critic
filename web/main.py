ae9ec3fbb92f3e17c9dcb1701e71c6ab93461068
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-05-01 14:20:13 -0400

    refactor: remove auth for post route

--- web/main.py
+++ web/main.py
@@ -12,25 +12,25 @@ auth_password = os.getenv("ADMIN_PASSWORD", None)
 auth_required = os.getenv("AUTH_REQUIRED", "false").lower() in ["true", "t"]
 
 
-def get_admin_username(credentials: HTTPBasicCredentials = Depends(security)) -> str:
-    if not auth_required:
-        return "guestuser"
-    correct_username = secrets.compare_digest(credentials.username, auth_user)
-    correct_password = secrets.compare_digest(credentials.password, auth_password)
-    if auth_required and not (correct_username and correct_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
-            headers={"WWW-Authenticate": "Basic"},
-        )
-    return credentials.username
+# def get_admin_username(credentials: HTTPBasicCredentials = Depends(security)) -> str:
+#     if not auth_required:
+#         return "guestuser"
+#     correct_username = secrets.compare_digest(credentials.username, auth_user)
+#     correct_password = secrets.compare_digest(credentials.password, auth_password)
+#     if auth_required and not (correct_username and correct_password):
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail="Incorrect email or password",
+#             headers={"WWW-Authenticate": "Basic"},
+#         )
+#     return credentials.username
 
 @app.get("/")
 def test():
     return {"status": "ok"}
 
 @app.post("/analysis")
-def analyze_code(code: str, username: str = Depends(get_admin_username)):
+def analyze_code(code: str): # , username: str = Depends(get_admin_username)):
     print(analyze_code_with_gpt3(code))
     return {"analysis": "chill"}
 
