3d05b76eabf0a63a1d8ee45d425faa0e6c861923
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2023-05-01 14:52:44 -0400

    feat: error handling for endpoint

--- web/main.py
+++ web/main.py
@@ -1,6 +1,6 @@
 import os
 import secrets
-from code_checker import get_sarif_report
+from code_checker import get_sarif_report, is_valid_key
 
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
@@ -41,9 +41,42 @@ def test():
 
 @app.post("/analyze")
 def analyze_code(code_changes: CodeChanges): # , username: str = Depends(get_admin_username)):
+    # check if key and files are present
+    if not (code_changes.key and code_changes.files):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Key and files are required",
+        )
+    
+    # check if file limit is not exceeded
+    if len(code_changes.files) > 10:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Maximum 10 files are allowed",
+        )
+    
+    # check that each file is valid
+    for file in code_changes.files:
+        if not (file.name and file.code):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Name and code are required for each file",
+            )
+        
+    # check that key is valid
+    if not is_valid_key(code_changes.key):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid key",
+        )
+    
+    # get SARIF report
     code_strings = {file.name: file.code for file in code_changes.files}
     analysis = get_sarif_report(code_changes.key, code_strings)
+
+    # TODO: remove this after testing
     print(analysis)
+    
     return {"analysis": analysis}
 
 if __name__ == "__main__":
